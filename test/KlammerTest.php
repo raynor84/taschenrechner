9dea4900bcea9fdabafaa33c57e543e9c34348c6
steve84 <stefanfischer84@gmail.com>
2021-09-08 15:49:45 +0200

    test Term 5 Mal Klammer 5 Minus Klammer 5 Plus 5 KlammerZu KlammerZu Equals 5*(5-10) correct test function result

--- test/KlammerTest.php
+++ test/KlammerTest.php
@@ -16,21 +16,33 @@ final class KlammerTest extends TestCase
         $this->numeric = $this->createMock(Numeric::class);
     }
     
-    public function testTermKlammer5Minus5KlammerZuMinus5EqualsMinus5(): void
+    public function testTermKlammer5Minus5KlammerZuMinus5EqualsKlammer5Minus5KlammerZuMinus5(): void
     {
         $this->operation = new Klammer($this->calculator, $this->numeric);
         $this->numeric->method('concatinateNumericValues')->willReturn(array("(","5","-","5",")","-", "5"));
         $this->calculator->method('calculate')->willReturn("0");
+        
         $this->assertSame("0-5", $this->operation->findAndCalculateTerm("(5-5)-5"));
 
     }
     
-    public function testTerm5MalKlammer5Minus5KlammerZuEquals0(): void
+    public function testTerm5MalKlammer5Minus5KlammerZuEquals5MalKlammer5Minus5KlammerZu(): void
     {
         $this->operation = new Klammer($this->calculator, $this->numeric);
         $this->numeric->method('concatinateNumericValues')->willReturn(array("5", "*","(","5","-","5",")"));
         $this->calculator->method('calculate')->willReturn("0");
+        
         $this->assertSame("5*0", $this->operation->findAndCalculateTerm("5*(5-5)"));
+
+    }
+    public function testTerm5MalKlammer5MinusKlammer5Plus5KlammerZuKlammerZuEquals5MalKlammer5Minus10KlammerZuKlammerZu(): void
+    {
+        $this->operation = new Klammer($this->calculator, $this->numeric);
+        $this->numeric->method('concatinateNumericValues')->willReturn(array("5", "*","(","5","-","(","5", "+", "5", ")",")"));
+        $this->calculator->method('calculate')->willReturn("(5-10)");
+
+
+        $this->assertSame("5*(5-10)", $this->operation->findAndCalculateTerm("5*(5-(5+5))"));
 
     }
 
