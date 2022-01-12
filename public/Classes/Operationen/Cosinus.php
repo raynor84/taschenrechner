b64cc11354fa791583d27e8f29cab2dd4b3e62be
raynor84 <stefanfischer84@gmail.com>
2022-01-12 19:38:59 +0100

    create Cosinus class

---
+++ public/Classes/Operationen/Cosinus.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Taschenrechner\Classes\Operationen;
+class Cosinus extends Operation {
+    public function getSign() {
+        return "cos(";
+    }
+
+    public function calculate($a, $b=NULL) {
+        return cos(deg2rad($a));
+    }
+
+    public function findAndCalculateTerm($term) {
+            //arbeitet intern mit dem Array
+			$array = preg_split('/(?<!^)(?!$)/u', $term );
+		   	
+			//füge alle darauf folgenden nummerischen werte zusammen
+            $array = $this->concatinator->concatinateArray($array);
+            //TODO change numeric to concatinator
+			//$array = $this->concatinator->concatinateNumericValues($array);
+            //$array = $this->concatinator->concatinateOperations($array);
+            for($i =0; $i < sizeof($array)-1; $i++) {
+                if($array[$i]=="cos(") {
+                    $array[$i] = $this->calculate($array[$i+1]);
+                    unset($array[$i+1]);
+                    unset($array[$i+2]);
+                    $array = array_values($array);
+                }
+            }
+            return implode("", $array);
+    }
+}
