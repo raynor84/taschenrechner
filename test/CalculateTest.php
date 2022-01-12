7ef9ec207af6aa0eea02a4b8a2859e569ff06df0
raynor84 <stefanfischer84@gmail.com>
2022-01-12 21:50:13 +0100

    test cos

--- test/CalculateTest.php
+++ test/CalculateTest.php
@@ -87,6 +87,10 @@ final class CalculateTest extends TestCase
 			"Term (9*3) sqrt 3 equals 3" => array("3√(9*3)", "3"),
 			"Term sin(2) equals 0.034899496702501" => array("sin(2)", "0.034899496702501"),
 			"Term 2*sin(2) equals 0.069798993405002" => array("2*sin(2)", "0.069798993405002"),
+			"Term sin(2)*2 equals 0.069798993405002" => array("sin(2)*2", "0.069798993405002"),
+			"Term cos(2) equals 0.9993908270191" => array("cos(2)", "0.9993908270191"),
+			"Term 2*cos(2) equals 1.9987816540382" => array("2*cos(2)", "1.9987816540382"),
+			"Term cos(2)*2 equals 1.9987816540382" => array("cos(2)*2", "1.9987816540382"),
         );
     }
 
