7ae2403dd158ea344aa9b9ec2943b87b75952adc
raynor84 <stefanfischer84@gmail.com>
2022-01-13 17:42:27 +0100

    fix ui

--- public/rechne.php
+++ public/rechne.php
@@ -4,12 +4,14 @@
 	use Taschenrechner\Classes\Operationen\Klammer;
 	use Taschenrechner\Classes\Operationen\KlammerZu;
 	use Taschenrechner\Classes\Operationen\Wurzel;
+	use Taschenrechner\Classes\Operationen\Cosinus;
+	use Taschenrechner\Classes\Operationen\Sinus;
 	use Taschenrechner\Classes\Operationen\Potenz;
 	use Taschenrechner\Classes\Operationen\Multiplikation;
 	use Taschenrechner\Classes\Operationen\Division;
 	use Taschenrechner\Classes\Operationen\Addition;
 	use Taschenrechner\Classes\Operationen\Subtraktion;
-	use Taschenrechner\Classes\Numeric;
+	use Taschenrechner\Classes\Concatinator;
 
 	session_start();
 	unset($_SESSION["exception"]);
@@ -24,14 +26,16 @@
 		//Füge Operationen hinzu:
 		//Deklariere einen Term und übergebe den Term
 		
-		$calculator->addOperation(4, new Klammer($calculator, new Numeric()));
-		$calculator->addOperation(4, new KlammerZu($calculator, new Numeric()));
-		$calculator->addOperation(3, new Wurzel($calculator, new Numeric()));
-		$calculator->addOperation(3, new Potenz($calculator, new Numeric()));
-		$calculator->addOperation(2, new Multiplikation($calculator, new Numeric()));
-		$calculator->addOperation(2, new Division($calculator, new Numeric()));
-		$calculator->addOperation(1, new Addition($calculator, new Numeric()));
-		$calculator->addOperation(1, new Subtraktion($calculator, new Numeric()));
+		$calculator->addOperation(4, new Sinus($calculator, new Concatinator()));
+		$calculator->addOperation(4, new Wurzel($calculator, new Concatinator()));
+		$calculator->addOperation(4, new Klammer($calculator, new Concatinator()));
+		$calculator->addOperation(4, new KlammerZu($calculator, new Concatinator()));
+		$calculator->addOperation(3, new Cosinus($calculator, new Concatinator()));
+		$calculator->addOperation(3, new Potenz($calculator, new Concatinator()));
+		$calculator->addOperation(2, new Multiplikation($calculator, new Concatinator()));
+		$calculator->addOperation(2, new Division($calculator, new Concatinator()));
+		$calculator->addOperation(1, new Addition($calculator, new Concatinator()));
+		$calculator->addOperation(1, new Subtraktion($calculator, new Concatinator()));
 		
 		
 		$ergebnis = $calculator->calculate($term);
